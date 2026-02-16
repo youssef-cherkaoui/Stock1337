c818499103f9240e91dcceb2dc006d07e8c03fc8
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-16 12:44:31 +0000

    init commit

---
+++ src/main/java/stock1337/stock1337/Stock1337Application.java
@@ -0,0 +1,13 @@
+package stock1337.stock1337;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stock1337Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stock1337Application.class, args);
+	}
+
+}
---
+++ src/test/java/stock1337/stock1337/Stock1337ApplicationTests.java
@@ -0,0 +1,13 @@
+package stock1337.stock1337;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stock1337ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
