f4b02a17a75e2597da4a0ed3339cd60c91af7358
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-17 15:48:13 +0000

    App

--- src/main/java/stock1337/stock1337/Stock1337Application.java
+++ src/main/java/stock1337/stock1337/Stock1337Application.java
@@ -2,8 +2,10 @@ package stock1337.stock1337;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("Model")
 public class Stock1337Application {
 
 	public static void main(String[] args) {
