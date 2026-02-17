6d414b57e34276fc5cf91f1f83bf884e88616c09
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-17 15:51:03 +0000

    Auth

---
+++ src/main/java/DTO/RegisterRequest.java
@@ -0,0 +1,19 @@
+package DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String Name;
+    private String Email;
+    private String Password;
+
+}
