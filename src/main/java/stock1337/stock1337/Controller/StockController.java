8712d6891e2cbcc6810941cd0f8e8abaf35b01fe
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-23 15:56:41 +0000

    update Controller

---
+++ src/main/java/stock1337/stock1337/Controller/StockController.java
@@ -0,0 +1,45 @@
+package stock1337.stock1337.Controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import stock1337.stock1337.Model.Stock;
+import stock1337.stock1337.Service.StockService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/auth/stocks")
+@RequiredArgsConstructor
+public class StockController {
+
+    private final StockService stockService;
+
+    @RequestMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<List<Stock>> getAll() {
+        return ResponseEntity.ok(stockService.getAllStocks());
+    }
+
+
+    @GetMapping("departement/{id}")
+    public ResponseEntity<List<Stock>> getByDept(
+            @PathVariable Long id
+    ){
+        return ResponseEntity.ok(stockService.getStockByDepartement(id));
+    }
+
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<Stock> updateStock(
+            @PathVariable Long id,
+            @RequestBody Stock stock
+    ){
+        return ResponseEntity.ok(stockService.updateStock(id, stock));
+    }
+
+
+
+}
