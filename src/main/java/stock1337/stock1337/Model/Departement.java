00a6e97d2c2cfcfb141a416b4cc893688748ec82
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-23 15:53:36 +0000

    update Models

---
+++ src/main/java/stock1337/stock1337/Model/Departement.java
@@ -0,0 +1,21 @@
+package stock1337.stock1337.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Departement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
