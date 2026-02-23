5fab90538d4955da75bd73c3dd3d6c04c372d665
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-23 15:58:35 +0000

    update repositories

--- src/main/java/stock1337/stock1337/Repository/DemandeRepository.java
+++ src/main/java/stock1337/stock1337/Repository/DemandeRepository.java
@@ -1,8 +1,15 @@
 package stock1337.stock1337.Repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import stock1337.stock1337.Enums.StatutDemande;
+import stock1337.stock1337.Model.Demande;
+
+import java.util.List;
 
 @Repository
-public interface DemandeRepository {
+public interface DemandeRepository extends JpaRepository<Demande, Long> {
+    List<Demande> findByStatut(StatutDemande status);
+    List<Demande> findByUserId(Long idUser);
 }
