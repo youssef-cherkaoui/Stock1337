d1ec1b5e971f05c7789bc5ec0cda60ba23fa61d1
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-23 15:58:23 +0000

    update repositories

--- src/main/java/stock1337/stock1337/Repository/ArticleRepository.java
+++ src/main/java/stock1337/stock1337/Repository/ArticleRepository.java
@@ -1,8 +1,20 @@
 package stock1337.stock1337.Repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import stock1337.stock1337.Model.Article;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface ArticleRepository {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    Optional<Article> findByName(String name);
+    List<Article> findByDepartementId(Long departementId);
+
+    @Query("select a from Article a where a.quantity <= a.minThreshold")
+    List<Article> FindLowStockArticles();
 }
