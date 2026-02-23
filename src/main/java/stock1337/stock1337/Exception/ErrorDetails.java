a54cc50941028d8d5b52e0f5ff493a1ac67cbad9
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-23 15:54:06 +0000

    Exceptions

---
+++ src/main/java/stock1337/stock1337/Exception/ErrorDetails.java
@@ -0,0 +1,15 @@
+package stock1337.stock1337.Exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
