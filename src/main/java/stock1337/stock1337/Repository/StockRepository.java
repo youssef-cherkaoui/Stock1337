09b664209c9c18b70f42114a0322bb76ddde1fe2
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-23 15:59:08 +0000

    update repositories

--- src/main/java/stock1337/stock1337/Repository/StockRepository.java
+++ src/main/java/stock1337/stock1337/Repository/StockRepository.java
@@ -1,7 +1,13 @@
 package stock1337.stock1337.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import stock1337.stock1337.Model.Stock;
+
+import java.util.List;
+
 @Repository
-public interface StockRepository {
+public interface StockRepository extends JpaRepository<Stock, Long> {
+    List<Stock> findByDepartementId(Long departementId);
 }
