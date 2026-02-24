74f6a970882e1d1389741d50e1654399f5151d77
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-24 17:26:40 +0000

    Update Service

--- src/main/java/stock1337/stock1337/Service/StockService.java
+++ src/main/java/stock1337/stock1337/Service/StockService.java
@@ -1,5 +1,6 @@
 package stock1337.stock1337.Service;
 
+import stock1337.stock1337.DTO.StockRequest;
 import stock1337.stock1337.Model.Stock;
 
 import java.util.List;
@@ -8,5 +9,6 @@ public interface StockService {
 
     List<Stock> getStockByDepartement(Long departementId);
     List<Stock> getAllStocks();
-    Stock updateStock(Long id, Stock stockDetails);
+    Stock updateStock(Long id, StockRequest request);
+    Stock createStock(StockRequest request);
 }
