cd18ae331c00efcee29bc49a56e9d2b659ce7a61
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-24 17:29:51 +0000

    Update Service

--- src/main/java/stock1337/stock1337/Service/Impl/StockServiceImpl.java
+++ src/main/java/stock1337/stock1337/Service/Impl/StockServiceImpl.java
@@ -3,10 +3,14 @@ package stock1337.stock1337.Service.Impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import stock1337.stock1337.DTO.StockRequest;
+import stock1337.stock1337.Model.Departement;
 import stock1337.stock1337.Model.Stock;
+import stock1337.stock1337.Repository.DepartementRepository;
 import stock1337.stock1337.Repository.StockRepository;
 import stock1337.stock1337.Service.StockService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,6 +18,7 @@ import java.util.List;
 public class StockServiceImpl implements StockService {
 
     private final StockRepository stockRepository;
+    private final DepartementRepository departementRepository;
 
     @Override
     public List<Stock> getStockByDepartement(Long departementId) {
@@ -26,13 +31,34 @@ public class StockServiceImpl implements StockService {
     }
 
     @Override
-    public Stock updateStock(Long id, Stock stockDetails) {
+    public Stock updateStock(Long id, StockRequest request) {
 
         Stock stock = stockRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Stock not found"));
 
-        stock.setQuantity(stockDetails.getQuantity());
-        stock.setLocalisation(stockDetails.getLocalisation());
+
+        if(request.getName() != null) stock.setName(request.getName());
+        if(request.getLocalisation() != null) stock.setLocalisation(request.getLocalisation());
+        if(request.getDepartementId() != null) {
+            Departement dep = departementRepository.findById(request.getDepartementId())
+                    .orElseThrow(() -> new RuntimeException("Departement not found"));
+            stock.setDepartement(dep);
+        }
+
+        return stockRepository.save(stock);
+    }
+
+    @Override
+    public Stock createStock(StockRequest request) {
+
+        Departement dep = departementRepository.findById(request.getDepartementId()
+                ).orElseThrow(() -> new RuntimeException("Departement not found"));
+
+        Stock stock = new Stock();
+        stock.setName(request.getName());
+        stock.setLocalisation(request.getLocalisation());
+        stock.setDepartement(dep);
+        stock.setArticles(new ArrayList<>());
         return stockRepository.save(stock);
     }
 }
