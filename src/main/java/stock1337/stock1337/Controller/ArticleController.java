48773dad20a247f873fbfdc956962f38d3e23c59
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-02-24 17:19:21 +0000

    Update Controller

--- src/main/java/stock1337/stock1337/Controller/ArticleController.java
+++ src/main/java/stock1337/stock1337/Controller/ArticleController.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import stock1337.stock1337.DTO.ArticleRequest;
 import stock1337.stock1337.Model.Article;
 import stock1337.stock1337.Repository.ArticleRepository;
+import stock1337.stock1337.Service.ArticleService;
 
 import java.util.List;
 
@@ -16,25 +18,26 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
+    private final ArticleService articleService;
 
     @GetMapping("/search")
-    public ResponseEntity<List<Article>> getArticlesByDepartement(
+    public ResponseEntity<List<Article>> saerchArticles (
+            @RequestParam(required = false) Long stockId,
             @RequestParam(required = false) Long depaId){
-        if(depaId != null){
-            return ResponseEntity.ok(articleRepository.findByDepartementId(depaId));
-        }
-        return ResponseEntity.ok(articleRepository.findAll());
+
+
+        return ResponseEntity.ok(articleService.searchArticle(stockId, depaId));
     }
 
     @PostMapping("/add")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Article> addArticle(@RequestBody Article article){
-        return ResponseEntity.ok(articleRepository.save(article));
+    public ResponseEntity<Article> addArticle(@RequestBody ArticleRequest request){
+        return ResponseEntity.ok(articleService.createArticle(request));
     }
 
     @GetMapping("/Low-Stock")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<Article>> getLowStockArticles(){
-        return ResponseEntity.ok(articleRepository.FindLowStockArticles());
+        return ResponseEntity.ok(articleService.getLowStockArticles());
     }
 }
