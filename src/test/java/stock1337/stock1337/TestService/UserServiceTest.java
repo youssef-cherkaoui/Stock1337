a24fc3b6bd79d1b2507f8717e46c6399fd6e4182
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-03-02 17:37:52 +0000

    Tseting UserService

--- src/test/java/stock1337/stock1337/TestService/UserServiceTest.java
+++ src/test/java/stock1337/stock1337/TestService/UserServiceTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceTest {
+ class UserServiceTest {
 
 
     @Mock
