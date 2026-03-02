bcb496d28ba9d196a6c3bb74a65d35e41716a23a
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-03-02 17:45:39 +0000

    Testing Services

--- src/test/java/stock1337/stock1337/TestService/DemandeServiceTest.java
+++ src/test/java/stock1337/stock1337/TestService/DemandeServiceTest.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-public class DemandeServiceTest {
+ class DemandeServiceTest {
 
     @Mock
     private DemandeRepository demandeRepository;
