27389855c4119f5b48395a4b573f7a3f1b357062
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-03-30 10:25:29 +0100

    PROPERTIES

--- src/main/java/stock1337/stock1337/Stock1337Application.java
+++ src/main/java/stock1337/stock1337/Stock1337Application.java
@@ -3,9 +3,11 @@ package stock1337.stock1337;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("stock1337.stock1337.Model")
+@EntityScan("stock1337.stock1337.model")
+@EnableJpaRepositories("stock1337.stock1337.repository")
 public class Stock1337Application {
 
 	public static void main(String[] args) {
